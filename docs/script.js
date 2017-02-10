b99614f7bc535b66c7813427d77ae965ec91ed12
Winson <winson.luk@gmail.com>
2017-02-10 00:50:44 -0800

    first commit

---
+++ docs/script.js
@@ -0,0 +1,18 @@
+jQuery.extend(jQuery.easing,{
+    easeInOutExpo: function(x, t, b, c, d){
+        if (t==0) return b;
+        if (t==d) return b+c;
+        if ((t/=d/2) < 1) return c/2 * Math.pow(2, 10 * (t - 1)) + b;
+        return c/2 * (-Math.pow(2, -10 * --t) + 2) + b;
+    }
+});
+
+$(document).ready(function(){
+
+    $('a[href="#"], a[href="#about"], a[href="#video"], a[href="#journey"], a[href="#people"], a[href="#analysis"], a[href="#discussion"]').click(function(e){
+        e.preventDefault();
+        $('nav').removeClass('visible');
+        $('html,body').stop().animate({scrollTop: $('.'+$(this).data('scrollto')).offset().top }, 700, 'easeInOutExpo', function(){});
+    });
+
+});
